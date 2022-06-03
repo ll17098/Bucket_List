a6b7db3ba7e96f59bec568cca402362b27a095d8
ll17098 <103225810+ll17098@users.noreply.github.com>
2022-06-03 21:37:56 +1200

    created userINFO database

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 conn = sqlite3.connect('mydatabase.db') # Warning: This file is created in the current directory
-conn.execute("CREATE TABLE bucket (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)")
-conn.execute("INSERT INTO bucket (task,status) VALUES ('Climb a mountain',1)")
-conn.execute("INSERT INTO bucket (task,status) VALUES ('Get a Job',1)")
+conn.execute("CREATE TABLE IF NOT EXISTS bucket (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)")
+conn.execute("CREATE TABLE IF NOT EXISTS userInfo (id INTEGER PRIMARY KEY, username char(100) NOT NULL, password char(100) NOT NULL)")
+conn.execute("INSERT INTO userInfo(username, password) VALUES('admin', 'admin') ")
 conn.commit()
