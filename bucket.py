b697b71eefd069bba605f6cb36118837d95ca804
ll17098 <103225810+ll17098@users.noreply.github.com>
2022-06-06 20:39:40 +1200

    attempt at adding user info to database 2

--- bucket.py
+++ bucket.py
@@ -1,105 +1,105 @@
 import sqlite3
-from bottle import route, run, template, request, debug, error
+from bottle import route, run, template, request, debug, error, redirect, view
 
 
+#connects to databsse
+conn = sqlite3.connect("mydatabase.db")
 
-@route('/')
+#Login page / landing page
+@route("/")
 def login():
-    return template('templates/login.html')
+    checklogin()
+    return template("templates/login.html")
 
-@route('/checkLogin', method=['POST'])
+#validates a users login inout
+@route("/", method=["POST"])
 def checklogin():
     uname = request.GET.username.strip()
     pword = request.GET.password.strip()
-    conn = sqlite3.connect('mydatabase.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM userInfo WHERE username LIKE ? AND password LIKE ?", (uname, pword))
-    result = c.fetchall()
+    c.execute("SELECT username, password FROM userInfo WHERE username LIKE ? AND password LIKE ? ", (uname, pword))
+
     
-    if  result == uname and pword:
-        return 'logged in'
+    
 
-    else:
-        return 'Incorrect Login'
-
-
+#Records users new log in details into sql
 @route("/reg", method="GET")
 def register():
- if request.GET.save:
-    username = request.GET.Rusername.strip()
-    password = request.GET.Rpassword.strip()
+    if request.method =="GET":
+        return template("templates/reg.html")
 
-    conn = sqlite3.connect('mydatabase.db')
-    c = conn.cursor()
+    if request.GET.save and len(usernamecheck) == 0:
+        username = request.GET.username.strip()
+        password = request.GET.password.strip()
+        usernamecheck = c.execute("SELECT COUNT(*) FROM userInfo WHERE username LIKE ?", username=username)
+        
+        
+        c = conn.cursor()
+        c.execute("INSERT INTO userInfo (username, password) VALUES (?,?)", (username, password))
+        conn.commit()
+        return "registered<br><a href='/bucket'>Home</a>"
 
-    c.execute("INSERT INTO userInfo (username,password) VALUES (?, ?)", (username, password))
-    conn.commit()
-    c.close()
-    return 'registered'
- else:
-    return template('templates/reg.html')
+    elif request.GET.save and len(usernamecheck) > 0:
+        return "usename already Exists"
 
-@route('/bucket', method='GET')
+        
+        
+#Displays user"s bucket list / Home page
+@route("/bucket", method="GET")
 def bucket_list():
-    conn = sqlite3.connect('mydatabase.db')
     c = conn.cursor()
     c.execute("SELECT id, task, status FROM bucket")
     result = c.fetchall()
     c.close()
-    return template('templates/make_table', rows=result)
+    return template("templates/make_table", rows=result)
 
-@route('/new', method='GET')
+#Add a new task to the list
+@route("/new", method="GET")
 def new_item():
  if request.GET.save:
     new = request.GET.task.strip()
-
-    conn = sqlite3.connect('mydatabase.db')
     c = conn.cursor()
-
     c.execute("INSERT INTO bucket (task,status) VALUES (?,?)", (new, "Incomplete"))
     conn.commit()
     return bucket_list()
 
  else:
-     return template('templates/new_task.html')
+     return template("templates/new_task.html")
 
-@route('/edit', method='GET')
+#edits current task
+@route("/edit/<no:int>", method="GET")
 def edit_item(no):
-
     edit = request.GET.task.strip()
     status = request.GET.status.strip()
-
     if request.GET.save:
         if status == "Complete":
-            status = "Incomplete"
-
+            status = "Complete"
         else:
             status = "Incomplete"
             
-        conn = sqlite3.connect('mydatabase.db')
         c = conn.cursor()
         c.execute("UPDATE bucket SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
         conn.commit()
+        return bucket_list()
 
-        return '<p>The item number %s was successfully updated</p>' % no
-
+#deletes task
     elif request.GET.delete:
-        conn = sqlite3.connect('mydatabase.db')
         c = conn.cursor()
         c.execute("DELETE FROM bucket WHERE task = ? AND id = ?", (edit, no))
         conn.commit()
+        return bucket_list()
 
-        return '<p>The item number %s was successfully deleted</p>' % no
     else:
-        conn = sqlite3.connect('mydatabase.db')
         c = conn.cursor()
         c.execute("SELECT task FROM bucket WHERE id LIKE ?", (str(no)))
         cur_data = c.fetchone()
-        return template('templates/edit_task', old=cur_data, no=no)
+        return template("templates/edit_task", old=cur_data, no=no)
 
+
+#catach an error
 @error(404)
 def mistake404(code):
-    return 'Sorry, this page does not exist!'
+    return "Sorry, this page does not exist!"
 
 debug(True)
 run(reloader=True)
